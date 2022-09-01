ad4b94c0c490c1149b778e6e6393538a80f33859
Tomasz Kostur <tomasz.kostur@gmail.com>
2022-09-01 21:54:59 +0200

    Create vector of traits

--- src/blackboard_items.rs
+++ src/blackboard_items.rs
@@ -1,11 +1,18 @@
-pub mod blackboard_shapes {
+use core::fmt::Debug;
+
+pub mod blackboard {
+
     use std::str::FromStr;
+
+    use super::Pinnable;
     #[derive(Debug)]
     pub struct StickyNote {
         pub message: String,
         placement: Point,
         representation: Rectangle
     }
+
+    impl Pinnable for StickyNote{}
 
     #[derive(Debug)]
     struct Rectangle {
@@ -21,14 +28,26 @@ pub mod blackboard_shapes {
     pub fn new_note() -> StickyNote {
         StickyNote { message: String::from_str("Sticky note!").unwrap(), placement: Point{x:0, y:0}, representation: Rectangle { dx: 10, dy: 10 } }
     }
+
+
+}
+
+trait Pinnable: Debug {
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn new_sticky() {
-        let sn = blackboard_shapes::new_note();
+    fn test_new_sticky() {
+        let sn = blackboard::new_note();
         assert_eq!(sn.message, "Sticky note!");
+        println!("new sticky note: {:?}\n", sn);
+
+        let item1 = Box::new(blackboard::new_note());
+        let item2 = Box::new(blackboard::new_note());
+        let item3 = Box::new(blackboard::new_note());
+        let board: Vec::<Box<dyn Pinnable>> = vec![item1, item2, item3];
+        println!("The board is: {:?}", board);
     }
 }
