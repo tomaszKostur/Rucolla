e16e8e23f1227ce760bfde40dbf2b8e9ac439ebf
Tomasz Kostur <tomasz.kostur@gmail.com>
2022-09-17 10:21:17 +0200

    Finally crafted warp example with channel not mutex

--- examples/warp_research.rs
+++ examples/warp_research.rs
@@ -1,45 +1,30 @@
-use blackboard_lib::blackboard::{new_note, StickyNote};
-use warp::Filter;
+#![deny(warnings)]
 use tokio::sync::mpsc;
-
-async fn serve_the_board(tx: mpsc::Sender<String>) {
-    let rest_api = warp::path("note")
-        .and(warp::path::param())
-        .map(|param: String| {
-            let send_out = tx.send(param);
-            match send_out {
-                Ok(()) => {
-                    println!("sending done properly");
-                },
-                Err(e) => {
-                    println!("some err when sending to update blackboard");
-                }
-            }
-            "The retunr from rest_api"
-        });
-
-    warp::serve(rest_api).run(([127, 0, 0, 1], 6677)).await;
-    println!("huehue")
-}
-
-async fn board_keeper(rx: mpsc::Receiver<String>) {
-    let mut blackboard = Vec::<StickyNote>::new();
-    loop {
-        match rx.await {
-            Ok(message) => {
-                println!("blackboard updated by Sticky Note {:?}", message);
-                blackboard.push(new_note());
-            },
-            Err(e) => {
-                println!("Error while receiving message: {:?}", e);
-            }
-        }
-    }
-}
+use tokio::time;
+use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let (tx, mut rx) = mpsc::channel::<StickyNote>();
-    tokio::spawn(async move {serve_the_board(tx).await});
-    tokio::spawn(async move {board_keeper(rx).await});
+    let (tx, mut rx) = mpsc::channel::<usize>(10);
+    let routes = warp::any()
+        .and(warp::any().map(move || tx.clone()))
+        .and_then(do_some);
+
+    tokio::spawn(warp::serve(routes).run(([127, 0, 0, 1], 3030)));
+    tokio::spawn(async move {
+        loop {
+            println!("RX loop: {:?}", rx.recv().await)
+        }
+    });
+    time::sleep(time::Duration::from_secs(10)).await;
+}
+
+async fn do_some(
+    sender: tokio::sync::mpsc::Sender<usize>,
+) -> Result<impl warp::Reply, warp::Rejection> {
+    sender.send(888).await.unwrap();
+    Ok(warp::reply::with_status(
+        "reply from do_some",
+        warp::http::StatusCode::OK,
+    ))
 }
